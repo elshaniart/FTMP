b36023e76b0b5c5772769ca62ff1b49900653161
Art Elshani <elshaniart01@gmail.com>
2025-01-27 17:41:07 +0100

    supabase login typeshit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
+import { supabase } from "../supabaseClient";
+import { useState, useEffect } from "react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+import { Session } from "@supabase/supabase-js";
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
 
-  return (
-    <>
-      <h1 className="text-3xl">Fitness Tracker and Meal Planner App</h1>
-    </>
-  )
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
+
+  const signIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
+  if (!session) {
+    return (
+      <>
+        {/* <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} /> */}
+        <button onClick={signIn}>Sign in with google</button>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h2>{session?.user?.email}</h2>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
